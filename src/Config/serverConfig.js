456b923255db2e99c07aa960fdee1d19eb090877
Shubhamashish1310 <98949989+Shubhamashish1310@users.noreply.github.com>
2024-11-10 23:31:11 +0530

    eslint and prettier setup

--- src/Config/serverConfig.js
+++ src/Config/serverConfig.js
@@ -1,5 +1,5 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const PORT= process.env.PORT || 3100
+export const PORT = process.env.PORT || 3100;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import express from "express";
-import { PORT } from "./Config/serverConfig.js";
+import express from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+import { PORT } from './Config/serverConfig.js';
 
 const app = express();
 
-app.get("/", (req, res) => {    
-    res.status(200).json({
-        name: "slackchatpro",
-        version: "1.0.0",
-        owner:"Shubham Ashish"
-    });
+app.get('/', (req, res) => {
+  res.status(StatusCodes.OK).json({
+    name: 'slackchatpro',
+    version: '1.0.0',
+    owner: 'Shubham Ashish'
+  });
 });
 
 app.listen(PORT, () => {
-    console.log("Server is running on port " + PORT);
-})
+  console.log('Server is running on port ' + PORT);
+});
